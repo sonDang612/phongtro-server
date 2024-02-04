63a652159699b21225858d280291466b9b192df8
sonDang612 <sonhoangdang0612@gmail.com>
2024-02-05 04:19:53 +0700

    feat: create model and migrate to db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 require("dotenv").config();
 import cors from "cors";
 import initRoutes from "./src/routes";
+import connectDB from "./src/config/connectDB";
 const app = express();
 app.use(
   cors({
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 initRoutes(app);
+connectDB();
 
 const port = process.env.PORT || 8888;
 const listener = app.listen(port, () => {
